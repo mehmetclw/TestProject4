68ac3c6c1eb968b927d4eac4c3d468f49c1aa3d9
Mehmet Gul <mehmet@clarusway.com>
2022-12-21 21:49:38 -0500

    forget password imporoved

--- src/main/java/org/example/registration/ForgetPassword.java
+++ src/main/java/org/example/registration/ForgetPassword.java
@@ -3,5 +3,9 @@ package org.example.registration;
 public class ForgetPassword {
     public static void main(String[] args) {
         System.out.println("hey I forgot my password");
+        System.out.println("This is Jack did some additions");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("i");
+        }
     }
 }
