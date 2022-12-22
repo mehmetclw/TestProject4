242d0ab81d396b79a455f363ff8b429f96384792
Mehmet Gul <mehmet@clarusway.com>
2022-12-21 21:52:38 -0500

    update on ForgetPasswordd class

--- src/main/java/org/example/registration/ForgetPassword.java
+++ src/main/java/org/example/registration/ForgetPassword.java
@@ -3,5 +3,12 @@ package org.example.registration;
 public class ForgetPassword {
     public static void main(String[] args) {
         System.out.println("hey I forgot my password");
+        System.out.println("Mary has also updated the registration");
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("some others");
     }
 }
