73a6b7a9b5c482e01be8075af0d25f2d395f99db
aysbird <aysebirdal01@gmail.com>
2022-12-21 22:21:37 -0500

    new registiration code abdate by ayse

--- src/main/java/org/example/registration/NewRegistration.java
+++ src/main/java/org/example/registration/NewRegistration.java
@@ -1,7 +1,17 @@
 package org.example.registration;
 
+/**
+ * Date: 12/21/22
+ * Develope by ayse
+ */
 public class NewRegistration {
     public static void main(String[] args) {
         System.out.println(" I am the new guy");
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("this code generate by ayse");
+
+        }
     }
+
+
 }
