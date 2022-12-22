51831d23093dab43d84da3e49aac1c546e18c73a
Mehmet Gul <mehmet@clarusway.com>
2022-12-21 21:34:45 -0500

    Login implementation improved

--- src/main/java/org/example/LoginTest.java
+++ src/main/java/org/example/LoginTest.java
@@ -3,5 +3,8 @@ package org.example;
 public class LoginTest {
     public void loginTesting(){
         System.out.println("I am checking the login");
+        for(int i=0;i<5;i++){
+            System.out.println("Verify Login");
+        }
     }
 }
